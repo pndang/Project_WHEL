71987186cf4255612467522090c0143c0539f364
Phu Dang <dangnphu31@gmail.com>
2023-05-12 19:58:47 -0700

    add permutation tests

--- hypothesis_tests/permutation_test_util.py
+++ hypothesis_tests/permutation_test_util.py
@@ -1,5 +1,9 @@
+import pandas as pd
+import numpy as np
+import os
 
-def tvd_of_groups(df, groups, cats):
+
+def tvd_of_groups(df, groups, cats, show_steps=False):
 
     '''
         groups: the binary column
@@ -12,8 +16,24 @@ def tvd_of_groups(df, groups, cats):
     # Normalize each column
     distr = counts / counts.sum()
 
+    if show_steps:
+        print("\nSTEPS:")
+        print(counts)
+        print()
+        print(distr)
+        print()
+        print(distr.diff(axis=1))
+        print()
+        print(distr.diff(axis=1).iloc[:, -1])
+        print()
+        print(distr.diff(axis=1).iloc[:, -1].abs())
+        print()
+        print(distr.diff(axis=1).iloc[:, -1].abs().sum() / 2)
+        print("END\n")
+    
     # Compute and return the total variation distance
     return distr.diff(axis=1).iloc[:, -1].abs().sum() / 2
+
 
 def permutation_simulation(df, N, shuffle_column, cats_column, 
                            significance_level):
@@ -21,27 +41,30 @@ def permutation_simulation(df, N, shuffle_column, cats_column,
     '''
         Run N simulations under the null hypothesis (both distributions are
         from the same population, any observed difference is due to chance)
+
+        Return:
+        Test results (str): observed tvd, p-value, assessment of null hypothesis
+        results (list): N simulated test statistics under the null hypothesis
     '''
 
     results = []
     for _ in range(N):
         # Shuffle group column (to_shuffle_column)
         with_shuffled = df.assign(
-            shuffled=np.random.permutation(df[to_shuffle_column])
+            shuffled=np.random.permutation(df[shuffle_column])
         )
 
         # Compute and store the TVD
-        tvd = tvd_of_groups(df, groups='shuffled', cats=cats_column)
+        tvd = tvd_of_groups(with_shuffled, groups='shuffled', cats=cats_column,
+                            show_steps=True if _ == 1 else False)
         results.append(tvd)
 
-        # Calculate p-value and assess null hypothesis
-        observed = tvd_of_groups(df, groups=shuffle_column, cats=cats_column)
-        pval = (results >= observed).mean()
-        if pval < significance_level:
-            return f'P-value = {pval}, Reject null hypothesis at {significance_level}'
-        return f'P-value = {pval}, Fail to reject null hypothesis at {significance_level}'
-
-
-if __name__ == '__main__':
-    import pandas as pd
-    import numpy as np
+        if _ % 1000 == 0:
+            print(tvd)
+        
+    # Calculate p-value and assess null hypothesis
+    observed = tvd_of_groups(df, groups=shuffle_column, cats=cats_column)
+    pval = (results >= observed).mean()
+    if pval < significance_level:
+        return f'Obsersed TVD = {np.round(observed, 3)}, P-value = {pval}, Reject null hypothesis at {significance_level}', results
+    return f'Obsersed TVD = {np.round(observed, 3)}, P-value = {pval}, Fail to reject null hypothesis at {significance_level}', results
